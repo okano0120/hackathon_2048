8531ae8e5f39406642444d8807ce038194dc350c
okano0120 <okano0120@gmail.com>
2021-04-10 15:47:26 +0900

    変数名変更

--- main.py
+++ main.py
@@ -32,8 +32,8 @@ POSITION = {"x": 8, "y": 8}
 BORDER_WIDTH = 8
 NUMBER = 4
 LENGTH = SQUARE_LENGTH * NUMBER + BORDER_WIDTH * NUMBER
-CELL_COLOR = '#cbbeb5'
-BORDER_COLOR = '#b2a698'
+CELL_COLOR = '#eee4da'
+BORDER_COLOR = '#bbada0'
 PATTERNS = [[i, j] for i in range(4) for j in range(4)]
 
 def set_field():
@@ -56,88 +56,74 @@ def create_canvas():
 
 def operate(event):
   if event.keysym in list(CHANGE_MAP.keys()):
-    game.change_array(event.keysym)
+    game.move(event.keysym)
 
 class Game:
   def __init__(self):
-    self.array = [[None for i in range(4)] for j in range(4)]
+    self.tile_cols = [[None for i in range(4)] for j in range(4)]
     for i, j in random.sample(PATTERNS, 2):
-      self.array[j][i] = random.choice([2, 4])
+      self.tile_cols[j][i] = random.choice([2, 4])
     self.show_all()
 
-  def create_sample(self):
-    self.array[2][1] = 8
-    self.array[3][1] = 4
-    self.array[3][2] = 4
-    self.array[2][3] = 8
-    self.array[3][3] = 4 
-    self.array[3][0] = 4 
+  def move(self, key):
+    _tile_cols = copy.copy(self.tile_cols)
+    _tile_cols = self.change_direction('pre', key, _tile_cols)
+    _tile_cols = self.calc(_tile_cols)
+    _tile_cols = self.move_left(_tile_cols)
+    _tile_cols = self.change_direction('next', key, _tile_cols)
 
-  def change_array(self, key):
-    _array = copy.copy(self.array)
-    _array = self.change_direction('pre', key, _array)
-    _array = self.calc(_array)
-    _array = self.move_left(_array)
-    _array = self.change_direction('next', key, _array)
-    print("step")
-    print(_array)
-    print(self.array)
-    if _array != self.array:
-      _array = self.append_number(_array)
-      self.array = _array
+    if _tile_cols != self.tile_cols:
+      _tile_cols = self.append_number(_tile_cols)
+      self.tile_cols = _tile_cols
       self.show_all()
 
-  def is_empty(self, pair, array):
-    print(pair)
-    return array[pair[1]][pair[0]] == None
+  def is_empty(self, pair, tile_cols):
+    return tile_cols[pair[1]][pair[0]] == None
 
-  def append_number(self, array):
-    print(self.array)
-    empty_array = list(filter(lambda pair: self.is_empty(pair, array), list(PATTERNS)))
-    print(empty_array)
-    append_pos = random.choice(empty_array)
-    array[append_pos[1]][append_pos[0]] = random.choice([2, 4])
-    return array
+  def append_number(self, tile_cols):
+    empty_tile_cols = list(filter(lambda pair: self.is_empty(pair, tile_cols), list(PATTERNS)))
+    append_pos = random.choice(empty_tile_cols)
+    tile_cols[append_pos[1]][append_pos[0]] = random.choice([2, 4])
+    return tile_cols
     
-
-  def change_direction(self, stage, direction, array):
+  def change_direction(self, stage, direction, tile_cols):
     if (CHANGE_MAP[direction][stage] == 'right_rotate'):
-      return self.rotate(array, is_right=True)
+      return self.rotate(tile_cols, is_right=True)
     elif (CHANGE_MAP[direction][stage] == 'left_rotate'):
-      return self.rotate(array, is_right=False)
+      return self.rotate(tile_cols, is_right=False)
     elif (CHANGE_MAP[direction][stage] == 'reverse'):
-      return self.reverse(array)
+      return self.reverse(tile_cols)
     else:
-      return array
+      return tile_cols
 
-  def rotate(self, array, is_right=True):
-    _array = [[None for i in range(4)] for j in range(4)]
-    for i in range(len(array)):
-      for j in range(len(array[0])):
+  def rotate(self, tile_cols, is_right=True):
+    _tile_cols = [[None for i in range(4)] for j in range(4)]
+    for i in range(len(tile_cols)):
+      for j in range(len(tile_cols[0])):
         if is_right:
-          _array[i][len(array) - 1 - j] = array[j][i]
+          _tile_cols[i][len(tile_cols) - 1 - j] = tile_cols[j][i]
         else:
-          _array[len(array) - 1 - i][j] = array[j][i]
-    return _array
+          _tile_cols[len(tile_cols) - 1 - i][j] = tile_cols[j][i]
+    return _tile_cols
 
-  def reverse(self, array):
-    return list(map(lambda ele: self.reverse_item(ele) , array))
+  def reverse(self, tile_cols):
+    return list(map(lambda ele: self.reverse_item(ele) , tile_cols))
 
-  def reverse_item(self, array):
-    _array = copy.copy(array)
-    _array.reverse()
-    return _array
+  def reverse_item(self, tile_cols):
+    _tile_cols = copy.copy(tile_cols)
+    _tile_cols.reverse()
+    return _tile_cols
 
-  def move_left(self, array):
-    _array = []
-    for ele in array:
+  def move_left(self, tile_cols):
+    _tile_cols = []
+    for ele in tile_cols:
       cnt = ele.count(None)
       list_a_filtered = [x for x in ele if x] + [None for i in range(cnt)]
-      _array.append(list_a_filtered)
-    return _array
+      _tile_cols.append(list_a_filtered)
+    return _tile_cols
 
-  def calc(self, array):
-    return list(map(lambda ele: self.calc_ele(ele), array))
+  def calc(self, tile_cols):
+    return list(map(lambda ele: self.calc_ele(ele), tile_cols))
   
   def calc_ele(self, ele):
     cnt = ele.count(None)
@@ -155,14 +141,14 @@ class Game:
     canvas.delete("count_text")
     for i in range(4):
       for j in range(4):
-        if self.array[j][i] != None:
-          self.set_number(self.array[j][i], i, j)
+        if self.tile_cols[j][i] != None:
+          self.set_number(self.tile_cols[j][i], i, j)
 
   def set_number(self, num, x, y):
-    print(num, x, y)
     center_x = POSITION["x"] + BORDER_WIDTH * x + BORDER_WIDTH / 2 + SQUARE_LENGTH * x + SQUARE_LENGTH / 2
     center_y = POSITION["y"] + BORDER_WIDTH * y + BORDER_WIDTH / 2 + SQUARE_LENGTH * y + SQUARE_LENGTH / 2
-    canvas.create_text(center_x, center_y, text=num, justify="center", font=("", 70), tag="count_text")
+    font_size = round(30 + 40 / len(str(num)))
+    canvas.create_text(center_x, center_y, text=num, justify="center", font=("", font_size), tag="count_text")
   
 def play():
   global canvas
